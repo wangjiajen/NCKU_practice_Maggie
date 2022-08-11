6354f77acc38115b98bb6de3be857b4262ae2908
wangjiajen <wangmaggie228@gmail.com>
2022-08-11 13:23:06 +0800

    add test set

--- IMDB.py
+++ IMDB.py
@@ -5,16 +5,17 @@ import os
 
 imdb_dir = '/home/u108029050/m/aclImdb' # Data directory
 train_dir = os.path.join(imdb_dir, 'train') # Get the path of the train set
-
+test_dir = os.path.join(imdb_dir, 'test') # Get the path of the train set
 # Setup empty lists to fill
-labels = []
-texts = []
-
+labels_train = []
+texts_train = []
+labels_test = []
+texts_test = []
 # First go through the negatives, then through the positives
 for label_type in ['neg', 'pos']:
     # Get the sub path
     dir_name = os.path.join(train_dir, label_type)
-    print('loading ',label_type)
+    print('loading train ',label_type)
     # Loop over all files in path
     for fname in tqdm(os.listdir(dir_name)):
         
@@ -22,65 +23,60 @@ for label_type in ['neg', 'pos']:
         if fname[-4:] == '.txt':
             # Read the text file and put it in the list
             f = open(os.path.join(dir_name, fname))
-            texts.append(f.read())
+            texts_train.append(f.read())
             f.close()
             # Attach the corresponding label
             if label_type == 'neg':
-                labels.append(0)
+                labels_train.append(0)
             else:
-                labels.append(1)
-# %%
-len(labels), len(texts)
-
-# %%
-import numpy as np
-np.mean(labels)
-# %%
-print('Label',labels[24002])
-print(texts[24002])
-
-print('Label',labels[1])
-print(texts[1])
+                labels_train.append(1)
+for label_type in ['neg', 'pos']:
+    # Get the sub path
+    dir_name = os.path.join(test_dir, label_type)
+    print('loading test',label_type)
+    # Loop over all files in path
+    for fname in tqdm(os.listdir(dir_name)):
+        
+        # Only consider text files
+        if fname[-4:] == '.txt':
+            # Read the text file and put it in the list
+            f = open(os.path.join(dir_name, fname))
+            texts_test.append(f.read())
+            f.close()
+            # Attach the corresponding label
+            if label_type == 'neg':
+                labels_test.append(0)
+            else:
+                labels_test.append(1)
 # %%
 from keras.preprocessing.text import Tokenizer
 import numpy as np
 
-max_words = 10000 # We will only consider the 10K most used words in this dataset
+vocab_size = 10000 # We will only consider the 10K most used words in this dataset
 
-tokenizer = Tokenizer(num_words=max_words) # Setup
-tokenizer.fit_on_texts(texts) # Generate tokens by counting frequency
-sequences = tokenizer.texts_to_sequences(texts) # Turn text into sequence of numbers
-# %%
+tokenizer = Tokenizer(num_words=vocab_size) # Setup
+tokenizer.fit_on_texts(texts_train)
+tokenizer.fit_on_texts(texts_test) # Generate tokens by counting frequency
+sequences_test = tokenizer.texts_to_sequences(texts_train) 
+sequences_train = tokenizer.texts_to_sequences(texts_test)# Turn text into sequence of numbers
 word_index = tokenizer.word_index
-print('Token for "the"',word_index['the'])
-print('Token for "Movie"',word_index['movie'])
-print('Token for "generator"',word_index['generator'])
-
-# %%
-# Display the first 10 words of the sequence tokenized
-sequences[24002][:10]
 # %%
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 maxlen = 100 # Make all sequences 100 words long
-data = pad_sequences(sequences, maxlen=maxlen)
-print(data.shape) # We have 25K, 100 word sequences now
+data_test = pad_sequences(sequences_test, maxlen=maxlen)
+data_train = pad_sequences(sequences_train, maxlen=maxlen)
+labels_train = np.asarray(labels_train)
+labels_test = np.asarray(labels_test)
 # %%
-labels = np.asarray(labels)
-
 # Shuffle data
-indices = np.arange(data.shape[0])
+indices = np.arange(data_test.shape[0])
 np.random.shuffle(indices)
-data = data[indices]
-labels = labels[indices]
-
-training_samples = 20000  # We will be training on 10K samples
-validation_samples = 5000  # We will be validating on 10000 samples
-
-# Split data
-x_train = data[:training_samples]
-y_train = labels[:training_samples]
-x_val = data[training_samples: training_samples + validation_samples]
-y_val = labels[training_samples: training_samples + validation_samples]
+X_test = data_test[indices]
+y_test = labels_test[indices]
+indices = np.arange(data_train.shape[0])
+np.random.shuffle(indices)
+X_train = data_train[indices]
+y_train = labels_train[indices]
 # %%
 from keras.models import Sequential
 from keras.layers import Embedding, Flatten, Dense
@@ -88,7 +84,7 @@ from keras.layers import Embedding, Flatten, Dense
 embedding_dim = 50
 
 model = Sequential()
-model.add(Embedding(max_words, embedding_dim, input_length=maxlen))
+model.add(Embedding(vocab_size, embedding_dim, input_length=maxlen))
 model.add(Flatten())
 model.add(Dense(32, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
@@ -97,8 +93,8 @@ model.summary()
 model.compile(optimizer='adam',
               loss='binary_crossentropy',
               metrics=['acc'])
-history = model.fit(x_train, y_train,
+history = model.fit(X_train, y_train,
                     epochs=10,
                     batch_size=32,
-                    validation_data=(x_val, y_val))
+                    validation_data=0.2)
 # %%
