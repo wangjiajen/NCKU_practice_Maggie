bdb017e6d6b7a2957206ea7cbdca8b73bfa7f915
wangjiajen <wangmaggie228@gmail.com>
2022-08-14 19:06:45 +0800

    first

--- multi-label.py
+++ multi-label.py
@@ -9,11 +9,174 @@ from ast import literal_eval
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-# %%
+#In[]
 arxiv_data = pd.read_csv("https://github.com/soumik12345/multi-label-text-classification/releases/download/v0.2/arxiv_data.csv")
 arxiv_data.head()
-# %%
+# In[]
 print(f"There are {len(arxiv_data)} rows in the dataset.")
-# %%
+# In[] 數據重複
 total_duplicate_titles = sum(arxiv_data["titles"].duplicated())
 print(f"There are {total_duplicate_titles} duplicate titles.")
+# In[]
+arxiv_data = arxiv_data[~arxiv_data["titles"].duplicated()]
+print(f"There are {len(arxiv_data)} rows in the deduplicated dataset.")
+
+# 有些詞的出現次數為1
+print(sum(arxiv_data["terms"].value_counts() == 1))
+
+# 統計“terms”列中不同值的個數，不包括null值
+print(arxiv_data["terms"].nunique())
+# %%
+# Filtering the rare terms.
+arxiv_data_filtered = arxiv_data.groupby("terms").filter(lambda x: len(x) > 1)
+arxiv_data_filtered.shape
+# %%改成List[str]
+arxiv_data_filtered["terms"] = arxiv_data_filtered["terms"].apply(lambda x: literal_eval(x))
+arxiv_data_filtered["terms"].values[:5]
+# %%
+test_split = 0.1
+# Initial train and test split.
+train_df, test_df = train_test_split(arxiv_data_filtered,test_size=test_split,
+                                     stratify=arxiv_data_filtered["terms"].values,)
+# 將stratify=X就是按照X中的比例分配，將stratify=y就是按照y中的比例分配 
+# Splitting the test set further into validation
+# and new test sets.
+val_df = test_df.sample(frac=0.5)#就是抽取其中50%
+test_df.drop(val_df.index, inplace=True)#inplace=True：不創建新的對象，直接對原始對象進行修改；
+
+print(f"Number of rows in training set: {len(train_df)}")
+print(f"Number of rows in validation set: {len(val_df)}")
+print(f"Number of rows in test set: {len(test_df)}")
+# %%讓模型知道輸入張量是參差不齊的(建構不規則張量)
+terms = tf.ragged.constant(train_df["terms"].values)
+lookup = tf.keras.layers.StringLookup(output_mode="multi_hot")
+lookup.adapt(terms)#該層將分析數據集，確定單個字串標記的頻率，​​並從中創建一個詞彙表
+vocab = lookup.get_vocabulary()
+
+def invert_multi_hot(encoded_labels):
+    """Reverse a single multi-hot encoded label to a tuple of vocab terms."""
+    hot_indices = np.argwhere(encoded_labels == 1.0)[..., 0]
+    return np.take(vocab, hot_indices)
+
+print("Vocabulary:\n")
+print(vocab)
+# %% 0和1的給定標籤集
+sample_label = train_df["terms"].iloc[0]
+print(f"Original label: {sample_label}")
+
+label_binarized = lookup([sample_label])
+print(f"Label-binarized representation: {label_binarized}")
+#In[]得到序列長度的百分比
+train_df["summaries"].apply(lambda x: len(x.split(" "))).describe()
+
+# %%
+max_seqlen = 150
+batch_size = 128
+padding_token = "<pad>" #填充
+auto = tf.data.AUTOTUNE # GPU訓練的同時CPU可以準備數據，從而提升訓練流程的效率
+
+def make_dataset(dataframe, is_train=True):
+    labels = tf.ragged.constant(dataframe["terms"].values)
+    label_binarized = lookup(labels).numpy()
+    dataset = tf.data.Dataset.from_tensor_slices(
+        (dataframe["summaries"].values, label_binarized))
+    dataset = dataset.shuffle(batch_size * 10) if is_train else dataset
+    return dataset.batch(batch_size)
+
+# %%
+train_dataset = make_dataset(train_df, is_train=True)
+validation_dataset = make_dataset(val_df, is_train=False)
+test_dataset = make_dataset(test_df, is_train=False)
+# %%
+text_batch, label_batch = next(iter(train_dataset))
+
+for i, text in enumerate(text_batch[:5]):
+    label = label_batch[i].numpy()[None, ...]
+    print(f"Abstract: {text}")
+    print(f"Label(s): {invert_multi_hot(label[0])}")
+    print(" ")
+# %% 以數字形式表示,計算摘要中出現的唯一單詞的數量
+vocabulary = set()
+train_df["summaries"].str.lower().str.split().apply(vocabulary.update)
+vocabulary_size = len(vocabulary)
+print(vocabulary_size)
+
+# %%
+text_vectorizer = layers.TextVectorization(max_tokens=vocabulary_size, ngrams=2, output_mode="tf_idf")
+# TextVectorization 層需要根據我們訓練集中的詞彙進行調整
+with tf.device("/CPU:0"):
+    text_vectorizer.adapt(train_dataset.map(lambda text, label: text))
+
+train_dataset = train_dataset.map(
+    lambda text, label: (text_vectorizer(text), label), num_parallel_calls=auto
+).prefetch(auto)
+validation_dataset = validation_dataset.map(
+    lambda text, label: (text_vectorizer(text), label), num_parallel_calls=auto
+).prefetch(auto)
+test_dataset = test_dataset.map(
+    lambda text, label: (text_vectorizer(text), label), num_parallel_calls=auto
+).prefetch(auto)
+# %%
+def make_model():
+    shallow_mlp_model = keras.Sequential(
+        [
+            layers.Dense(512, activation="relu"),
+            layers.Dense(256, activation="relu"),
+            layers.Dense(lookup.vocabulary_size(), activation="sigmoid"),
+        ]  # More on why "sigmoid" has been used here in a moment.
+    )
+    return shallow_mlp_model
+# %%
+epochs = 20
+
+shallow_mlp_model = make_model()
+shallow_mlp_model.compile(
+    loss="binary_crossentropy", optimizer="adam", metrics=["categorical_accuracy"]
+)
+
+history = shallow_mlp_model.fit(
+    train_dataset, validation_data=validation_dataset, epochs=epochs
+)
+
+
+def plot_result(item):
+    plt.plot(history.history[item], label=item)
+    plt.plot(history.history["val_" + item], label="val_" + item)
+    plt.xlabel("Epochs")
+    plt.ylabel(item)
+    plt.title("Train and Validation {} Over Epochs".format(item), fontsize=14)
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+plot_result("loss")
+plot_result("categorical_accuracy")
+# %%
+_, categorical_acc = shallow_mlp_model.evaluate(test_dataset)
+print(f"Categorical accuracy on the test set: {round(categorical_acc * 100, 2)}%.")
+# %%
+# Create a model for inference.
+model_for_inference = keras.Sequential([text_vectorizer, shallow_mlp_model])
+
+# Create a small dataset just for demoing inference.
+inference_dataset = make_dataset(test_df.sample(100), is_train=False)
+text_batch, label_batch = next(iter(inference_dataset))
+predicted_probabilities = model_for_inference.predict(text_batch)
+
+# Perform inference.
+for i, text in enumerate(text_batch[:5]):
+    label = label_batch[i].numpy()[None, ...]
+    print(f"Abstract: {text}")
+    print(f"Label(s): {invert_multi_hot(label[0])}")
+    predicted_proba = [proba for proba in predicted_probabilities[i]]
+    top_3_labels = [
+        x
+        for _, x in sorted(
+            zip(predicted_probabilities[i], lookup.get_vocabulary()),
+            key=lambda pair: pair[0],
+            reverse=True,
+        )
+    ][:3]
+    print(f"Predicted Label(s): ({', '.join([label for label in top_3_labels])})")
+    print(" ")
